43f799251caf29576c5a4fc0b504b5899a479e22
JT <49847213+401Unauth@users.noreply.github.com>
2019-04-23 18:38:30 -0400

    Update palindromeChecker.py

--- palindromeChecker.py
+++ palindromeChecker.py
@@ -1,10 +1,9 @@
-def palindromeCheck(string): #Begins function
+def palindromeCheck(string):
   reversed = string[::-1] #string[::-1] Reverses string. Found at https://www.geeksforgeeks.org/reverse-string-python-5-different-ways/
-  if string == reversed: #Begins if tag; checks to see if the original string is equal to itself backwards
+  if string == reversed:
     print(string + " is a palindrome!") #Output if it is a palindrome
-  else: #Begins else tag, matches everything that is not a palindrome
+  else:
     print(string + " is not a palindrome!") #Output if it is not a palindrome
 
-
-my_function("palindrome") #Sample non-palindrome
-my_function("racecar") #Sample palindrome
+palindromeCheck("palindrome") #Sample non-palindrome
+palindromeCheck("racecar") #Sample palindrome
